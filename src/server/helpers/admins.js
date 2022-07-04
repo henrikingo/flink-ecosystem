66093b2c8517b12ac14589c9a85bb320783440e7
Henrik Ingo <henrik.ingo@avoinelama.fi>
2022-07-04 20:17:06 +0300

    Define first Cassandra admins
    Removes id's that are admins in flink-packages.org.

--- src/server/helpers/admins.js
+++ src/server/helpers/admins.js
@@ -2,23 +2,17 @@
 // To find someone's ID from their username you can use this:
 //    curl -s https://github.com/$username | awk -F '\/|[?]s=' '/og:image/ {print $5}'
 export const admins = [
-  1727146, // StephanEwen
-  5756858, // tillrohrmann
-  1756620, // uce
-  2388347, // fhueske
-  5725237, // zentol
-  68551, // aljoscha
-  89049, // rmetzger
-  11538663, // knaufk
-  1891970, // sjwiesman
-  6242259, // dawidwys
+  413669, // pmcfadin
+  2482018, // henrikingo
+  87622610, // aar0np
+  89990471, // vmic2002
 ];
 
-if (process.env.NODE_ENV !== "production") {
-  admins.push(
-    588852 // sorahn
-  );
-}
+// if (process.env.NODE_ENV !== "production") {
+//   admins.push(
+//     588852 // sorahn
+//   );
+// }
 
 export const checkAccess = (ctx, id) => {
   const canAccess =
